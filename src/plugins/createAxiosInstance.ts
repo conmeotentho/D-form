0c47159c726397ad279b130666a139c020d97792
TuNguyen <anhtu_nguyen@vnpt.vn>
2025-10-24 11:20:52 +0700

    build

--- src/plugins/createAxiosInstance.ts
+++ src/plugins/createAxiosInstance.ts
@@ -18,8 +18,21 @@ export function createAxiosInstance(BASEURL: string) {
 
   // Request interceptor for API calls
   axiosInstance.interceptors.request.use((config: any) => {
-    const access_token = localStorage.getItem('access_token');
-    const refresh_token = localStorage.getItem('refresh_token');
+    const lowcoderStorage = localStorage.getItem('lowcoder_app_local_storage');
+    let access_token = null;
+    let refresh_token = null;
+
+    if (lowcoderStorage) {
+      try {
+        const parsed = JSON.parse(lowcoderStorage);
+        access_token = parsed?.access_token || null;
+        refresh_token = parsed?.refresh_token || null;
+      } catch (e) {
+        console.warn('Không parse được lowcoder_app_local_storage:', e);
+      }
+    }
+    access_token = access_token || localStorage.getItem('access_token');
+    refresh_token = refresh_token  || localStorage.getItem('refresh_token');
     if (access_token) {
       config.headers = {
         ...config.headers,
